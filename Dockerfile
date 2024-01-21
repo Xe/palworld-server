385605ea134d18ed7268feb083981019f076d9dd
Xe Iaso <me@xeiaso.net>
2024-01-21 11:50:02 -0500

    fix the server running
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM cm2network/steamcmd
+FROM cm2network/steamcmd:root
 
 COPY launch.sh /home/steam
+
+RUN apt-get update \
+ && apt-get install -y socat net-tools vim nano
+
+USER steam
 
 CMD ["/home/steam/launch.sh"]
--- launch.sh
+++ launch.sh
@@ -2,10 +2,17 @@
 
 set +x
 
+(
+    while true; do
+        socat -T 600 UDP4-LISTEN:8211,fork,reuseaddr,bind=fly-global-services UDP4:127.0.0.1:8212
+        sleep 2
+    done
+) &
+
 ./steamcmd.sh +quit
 
 while true; do
     sleep 2
     ./steamcmd.sh +force_install_dir /home/steam/palworld +login anonymous +app_update 2394010 validate +quit
-    (cd /home/steam/palworld && ./PalServer.sh port=8211 players=32)
+    (cd /home/steam/palworld && ./PalServer.sh port=8211 players=32 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS)
 done
